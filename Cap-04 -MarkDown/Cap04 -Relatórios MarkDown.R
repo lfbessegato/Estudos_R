75b5d1f63b714461dbb6f7dc7e0627d3181e2330
Luciano Bessegato <luciano@nt-luciano.localdomain.dns>
2022-10-25 22:17:30 -0300

    Cap-04 - Produzindo Relatórios com o R MarkDown

---
+++ Cap-04 -MarkDown/Cap04 -Relatórios MarkDown.R
@@ -0,0 +1,19 @@
+# Exemplo 01
+# Os códigos de R devem vir sempre numa janela que se destaca com fundo cinza 
+# claro, iniciando com três sinais de acento grave depois entre chaves inicia 
+# com a letra r minúscula, podendo ter alguns argumentos, depois os comandos do
+# R e finaliza novamente como no início com três sinais de acento grave.
+
+# Essas janelas de comando são chamadas de chunks e podem ser inseridas através
+# do botão Insert toda vez que você deseja inserir comandos do R para serem 
+# processados no seu documento.
+
+# Marcar a opção Include a table of contents => Para incluir os tópicos
+
+########## CRIANDO TÍTULOS
+# Título 1
+## Título 2
+### Título 3
+#### Título 4
+##### Título 5
+###### Título 6
